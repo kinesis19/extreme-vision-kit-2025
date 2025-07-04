7893c5693ae5035b74e5c5803af053cb2d90220b
kinesis19 <lordkinesis@naver.com>
2025-07-04 18:31:51 +0900

    :tada: Init: create a extreme-vision-kit-2025 package

---
+++ extreme-vision-kit-2025/src/main.cpp
@@ -0,0 +1,10 @@
+#include "extreme-vision-kit-2025/mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
---
+++ extreme-vision-kit-2025/src/mainwindow.cpp
@@ -0,0 +1,14 @@
+#include "extreme-vision-kit-2025/mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
